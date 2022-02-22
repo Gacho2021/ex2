f289aec5b5d6b529ba9ef60a69336741384c2e8e
Gacho2021 <87105994+Gacho2021@users.noreply.github.com>
2022-02-21 20:10:13 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+//made function to disable out the MPG box
+window.onload = function(){
+document.getElementById("MPG").disabled = true;
+}
+//Assigned the submit button a variable
+var Submit = document.getElementById("Submit")
+
+//attached the event listener to the submit button and coded the "annonymous" function.
+Submit.addEventListener("click", function() {
+let Miles = document.getElementById("Miles").value
+let Gas = document.getElementById("Gas").value
+Miles = parseInt(Miles)
+Gas = parseInt(Gas)
+let MPG = Miles / Gas
+document.getElementById("MPG").value = MPG
+}, false);
 
